68bef2415ecdea184f7bd54729e852d6240b1168
Exxarah <sarahbeavan@gmail.com>
2021-09-28 22:15:43 +1300

    Added a drag/delay on camera movement
    Is pretty janky atm tbh

--- Assets/CameraRotate.cs
+++ Assets/CameraRotate.cs
@@ -5,13 +5,16 @@ public class CameraRotate : MonoBehaviour
 {
     private const float MINDistance = 5f;
     private const float MAXDistance = 30f;
-    
+    private const float MINMagnitude = 1f;
+
     [SerializeField] public Transform objectToWatch;
     [SerializeField, Range(MINDistance, MAXDistance)] public float distanceFromObject;
     [SerializeField] public MouseButtons mouseButton;
     [SerializeField, Range(5, 30)] public float scrollSensitivity;
+    [SerializeField, Range(0f, 1f)] public float momentumDelay = 0.5f;
 
     private Vector3 _previousPosition;
+    private Vector3 _previousDirection;
     private Camera _camera;
 
     private void Start()
@@ -31,13 +34,15 @@ public class CameraRotate : MonoBehaviour
 
         if (Input.GetMouseButton((int)mouseButton))
         {
-            Vector3 direction = _previousPosition - _camera.ScreenToViewportPoint(Input.mousePosition);
+            _previousDirection = _previousPosition - _camera.ScreenToViewportPoint(Input.mousePosition);
 
-            _camera.transform.position = objectToWatch.position;
-            _camera.transform.Rotate(new Vector3(1, 0, 0), direction.y * 100);
-            _camera.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 100, Space.World);
-            _camera.transform.Translate(new Vector3(0, 0, -distanceFromObject));
-            _previousPosition = _camera.ScreenToViewportPoint(Input.mousePosition);
+            MoveCamera(_previousDirection);
+        }
+        else
+        {
+            // Decay Direction
+            _previousDirection *= momentumDelay;
+            MoveCamera(_previousDirection);
         }
 
         #endregion
@@ -48,5 +53,15 @@ public class CameraRotate : MonoBehaviour
         _camera.fieldOfView = Mathf.Clamp(_camera.fieldOfView, 10, 120);
 
         #endregion
+    }
+
+    private void MoveCamera(Vector3 direction)
+    {
+        _camera.transform.position = objectToWatch.position;
+            
+        _camera.transform.Rotate(new Vector3(1, 0, 0), direction.y * 100);
+        _camera.transform.Rotate(new Vector3(0, 1, 0), -direction.x * 100, Space.World);
+        _camera.transform.Translate(new Vector3(0, 0, -distanceFromObject));
+        _previousPosition = _camera.ScreenToViewportPoint(Input.mousePosition);
     }
 }
