347fce7c3d844982a915e225067d3d5c3d01b26c
Exxarah <sarahbeavan@gmail.com>
2021-10-25 15:38:10 +1300

    Added rotation script for documentation images

---
+++ Assets/RotateObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class RotateObject : MonoBehaviour
+    {
+        public float speedFactor;
+
+        public void Update()
+        {
+            this.transform.Rotate(0f, speedFactor * Time.deltaTime, 0f);
+        }
+    }
+}
